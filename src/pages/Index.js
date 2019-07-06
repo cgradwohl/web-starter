c926512762624315a0ce987d1bee30835e6e47b3
Chris Gradwohl <christophergradwohl@gmail.com>
2019-07-05 17:46:16 -0700

    updates index

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 
-import { Text } from 'rebass';
+import { Card, Text } from 'rebass';
 
 export default () => (
-  <Text fontFamily="main" fontSize={9}>
-    we lit!!lorem Lorem ipsum dolor sit amet consectetur adipisicing elit.
-    Nam ratione natus molestias dignissimos rem qui tempora amet molestiae
-    porro voluptatibus quae aliquam harum, esse culpa inventore quaerat
-    corporis non iusto?
+  <Card
+    fontSize={6}
+    width={[1, 1, 1 / 2]}
+    p={5}
+    my={5}
+    bg="primary"
+    borderRadius={8}
+    boxShadow="0 2px 16px rgba(0, 0, 0, 0.25)"
+  >
+    <Text fontFamily="main">
+      we lit!!lorem Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Nam ratione natus molestias dignissimos rem qui tempora amet molestiae
+      porro voluptatibus quae aliquam harum, esse culpa inventore quaerat
+      corporis non iusto?
 
-    lorem Lorem ipsum dolor sit amet consectetur adipisicing elit.
-    Nam ratione natus molestias dignissimos rem qui tempora amet molestiae
-    porro voluptatibus quae aliquam harum, esse culpa inventore quaerat
-    corporis non iusto?
-  </Text>
+      lorem Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Nam ratione natus molestias dignissimos rem qui tempora amet molestiae
+      porro voluptatibus quae aliquam harum, esse culpa inventore quaerat
+      corporis non iusto?
+    </Text>
+  </Card>
 );
