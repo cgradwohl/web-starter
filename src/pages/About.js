abe59b2dfc8a43d29ddaca01609398ee9841b8bb
Chris Gradwohl <christophergradwohl@gmail.com>
2019-07-05 19:40:15 -0700

    remove provider from coponents

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Button, Card, Flex } from 'rebass';
-import { CountProvider, useCount } from '../context/count';
+import { useCount } from '../context/count';
 
 const Counter = () => {
   const [count, setCount] = useCount();
@@ -14,23 +14,21 @@ const Counter = () => {
 };
 
 export default () => (
-  <CountProvider>
-    <Card
-      fontSize={6}
-      width={[1, 1, 1 / 2]}
-      p={5}
-      my={5}
-      bg="secondary"
-      borderRadius={8}
-      boxShadow="0 2px 16px rgba(0, 0, 0, 0.25)"
-    >
-      <FontAwesomeIcon icon={['fab', 'apple']} />
-      <FontAwesomeIcon icon={['far', 'smile']} />
-      <FontAwesomeIcon icon={['fab', 'github']} />
-      <FontAwesomeIcon icon={['fab', 'react']} />
-      <FontAwesomeIcon icon={['far', 'grin-alt']} />
-      <FontAwesomeIcon icon={['fas', 'cannabis']} />
-      <Counter />
-    </Card>
-  </CountProvider>
+  <Card
+    fontSize={6}
+    width={[1, 1, 1 / 2]}
+    p={5}
+    my={5}
+    bg="secondary"
+    borderRadius={8}
+    boxShadow="0 2px 16px rgba(0, 0, 0, 0.25)"
+  >
+    <FontAwesomeIcon icon={['fab', 'apple']} />
+    <FontAwesomeIcon icon={['far', 'smile']} />
+    <FontAwesomeIcon icon={['fab', 'github']} />
+    <FontAwesomeIcon icon={['fab', 'react']} />
+    <FontAwesomeIcon icon={['far', 'grin-alt']} />
+    <FontAwesomeIcon icon={['fas', 'cannabis']} />
+    <Counter />
+  </Card>
 );
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 import { Button, Card, Flex, Text } from 'rebass';
 
-import { UserProvider, useUser } from '../context/user';
-import { CountProvider, useCount } from '../context/count';
+import { useUser } from '../context/user';
+import { useCount } from '../context/count';
 
 const UserInfo = () => {
   const [user] = useUser();
@@ -25,60 +25,29 @@ const Counter = () => {
 };
 
 export default () => (
-  <UserProvider>
-    <Card
-      fontSize={6}
-      width={[1, 1, 1 / 2]}
-      p={5}
-      my={5}
-      bg="primary"
-      borderRadius={8}
-      boxShadow="0 2px 16px rgba(0, 0, 0, 0.25)"
-    >
-      <Text fontFamily="main">
-        we lit!!lorem Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Nam ratione natusdcdc molestias dignissimos rem qui tempora amet molestiae
-        porro voluptatibus quae aliquam harum, esse culpa inventore quaerat
-        corporis non iusto?
+  <Card
+    fontSize={6}
+    width={[1, 1, 1 / 2]}
+    p={5}
+    my={5}
+    bg="primary"
+    borderRadius={8}
+    boxShadow="0 2px 16px rgba(0, 0, 0, 0.25)"
+  >
+    <Text fontFamily="main">
+      we lit!!lorem Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Nam ratione natusdcdc molestias dignissimos rem qui tempora amet molestiae
+      porro voluptatibus quae aliquam harum, esse culpa inventore quaerat
+      corporis non iusto?
 
-        lorem Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Nam ratione natus molestias dignissimos rem qui tempora amet molestiae
-        porro voluptatibus quae aliquam harum, esse culpa inventore quaerat
-        corporis non iusto?
-      </Text>
-      <br />
-      <UserInfo />
-      <br />
-      <CountProvider>
-        <Counter />
-      </CountProvider>
-    </Card>
-    <Card
-      fontSize={6}
-      width={[1, 1, 1 / 2]}
-      p={5}
-      my={5}
-      bg="primary"
-      borderRadius={8}
-      boxShadow="0 2px 16px rgba(0, 0, 0, 0.25)"
-    >
-      <Text fontFamily="main">
-        we lit!!lorem Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Nam ratione natusdcdc molestias dignissimos rem qui tempora amet molestiae
-        porro voluptatibus quae aliquam harum, esse culpa inventore quaerat
-        corporis non iusto?
-
-        lorem Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Nam ratione natus molestias dignissimos rem qui tempora amet molestiae
-        porro voluptatibus quae aliquam harum, esse culpa inventore quaerat
-        corporis non iusto?
-      </Text>
-      <br />
-      <UserInfo />
-      <br />
-      <CountProvider>
-        <Counter />
-      </CountProvider>
-    </Card>
-  </UserProvider>
+      lorem Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Nam ratione natus molestias dignissimos rem qui tempora amet molestiae
+      porro voluptatibus quae aliquam harum, esse culpa inventore quaerat
+      corporis non iusto?
+    </Text>
+    <br />
+    <UserInfo />
+    <br />
+    <Counter />
+  </Card>
 );
